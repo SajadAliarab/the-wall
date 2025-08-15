9d7e13c47eba0e99bd928b0ac2398fc79a1e0b5a
Sajad Aliarab <sajad.aliarab@gmail.com>
2025-08-15 11:48:54 +0330

    feat(filament): install package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
     App\Providers\ApiResponseServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
 ];
