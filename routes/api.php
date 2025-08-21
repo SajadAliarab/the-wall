7ef5b6ee675a07263d5713e94b2e483a05fe769f
Sajad Aliarab <sajad.aliarab@gmail.com>
2025-08-21 23:37:43 +0100

    feat(Create Post): add CreatePost api route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\Auth\CreateTokenController;
 use App\Http\Controllers\Api\V1\Category\GetCategoryListController;
+use App\Http\Controllers\Api\V1\Post\CreatePostController;
 use App\Http\Controllers\Api\V1\User\CreateUserController;
 use App\Http\Controllers\Api\V1\User\UpdateUserController;
 
@@ -23,7 +24,7 @@ Route::name('api.')
                 Route::name('users.')
                     ->prefix('users')
                     ->group(function () {
-                        Route::post('create', CreateUserController::class)->name('create-user');
+                        Route::post('create', CreateUserController::class)->name('create');
 
                         Route::middleware('auth:sanctum')->group(function () {
                             Route::put('update', UpdateUserController::class)->name('update');
@@ -35,6 +36,15 @@ Route::name('api.')
                     ->prefix('categories')
                     ->group(function () {
                         Route::get('/categories', GetCategoryListController::class)->name('list');
+                    });
+
+                // Posts
+                Route::name('posts.')
+                    ->prefix('posts')
+                    ->group(function () {
+                        Route::middleware('auth:sanctum')->group(function () {
+                            Route::post('create', CreatePostController::class)->name('create');
+                        });
                     });
             });
     });
