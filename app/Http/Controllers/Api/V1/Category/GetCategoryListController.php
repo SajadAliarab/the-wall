a4fab11df780baae441123956efc397325c4ec5b
Sajad Aliarab <sajad.aliarab@gmail.com>
2025-08-22 23:04:36 +0100

    fix(Get Category) : add message and responseCode to result

--- app/Http/Controllers/Api/V1/Category/GetCategoryListController.php
+++ app/Http/Controllers/Api/V1/Category/GetCategoryListController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1\Category;
 use App\Actions\Api\V1\Category\GetCategoryListAction;
 use App\Http\Controllers\Api\ApiBaseController;
 use App\Http\Resources\CategoryResource;
+use Symfony\Component\HttpFoundation\Response;
 
 class GetCategoryListController extends ApiBaseController
 {
@@ -14,6 +15,8 @@ class GetCategoryListController extends ApiBaseController
 
         return response()->apiSuccess(
             data: CategoryResource::collection($categories),
+            messages: "Category list found.",
+            responseCode:Response::HTTP_OK
         );
     }
 }
