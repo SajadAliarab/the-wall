5ad3cec38134ef201dfd9332bab03592502a580a
Sajad Aliarab <sajad.aliarab@gmail.com>
2025-08-22 23:04:01 +0100

    fix(Creat User) : add db begin transaction put create query on try and catch because of send verify email

--- app/Actions/Api/V1/User/CreateUserAction.php
+++ app/Actions/Api/V1/User/CreateUserAction.php
@@ -5,17 +5,27 @@ namespace App\Actions\Api\V1\User;
 use App\DataTransferObject\User\CreateUserDto;
 use App\Models\User;
 use App\Notifications\CreateUserNotification;
+use Illuminate\Support\Facades\DB;
 
 class CreateUserAction
 {
     public function handle(CreateUserDto $dto): User
     {
-        $user = User::query()->create([
-            'name' => $dto->name,
-            'email' => $dto->email,
-            'password' => $dto->password,
-        ]);
-        $user->notify(new CreateUserNotification);
+        DB::beginTransaction();
+        try {
+            $user = User::query()->create([
+                'name' => $dto->name,
+                'email' => $dto->email,
+                'password' => $dto->password,
+            ]);
+            $user->notify(new CreateUserNotification);
+        }catch(throwable $th){
+            DB::rollBack();
+            throw $th;
+        }
+        DB::commit();
+
+
 
         return $user;
 
