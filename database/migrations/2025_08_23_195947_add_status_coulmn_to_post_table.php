bbe1d5b2f72b44e5cd7345204615f916ae14c45a
Sajad Aliarab <sajad.aliarab@gmail.com>
2025-08-23 21:35:56 +0100

    feat(Create Post) : add Status section for creating post with default pending and using enum

---
+++ database/migrations/2025_08_23_195947_add_status_coulmn_to_post_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Enums\PostStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->enum('status', PostStatus::toArray())
+                ->default(PostStatus::Pending)
+                ->after('category_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
