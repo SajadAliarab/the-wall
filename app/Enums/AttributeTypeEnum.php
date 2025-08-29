e4dd47281698265da9c22f1d8e9e3f923568b822
Hamid Dehnavi <hamid.dev.pro@gmail.com>
2025-08-29 12:14:59 +0330

    feat(post): wip
    Signed-off-by: Hamid Dehnavi <hamid.dev.pro@gmail.com>

--- app/Enums/AttributeTypeEnum.php
+++ app/Enums/AttributeTypeEnum.php
@@ -8,4 +8,14 @@ enum AttributeTypeEnum: string
     case STRING = 'string';
     case BOOLEAN = 'boolean';
     case LIST = 'list';
+
+    public function validationType(): string
+    {
+        return match ($this) {
+            self::NUMERIC => 'numeric',
+            self::STRING => 'string',
+            self::BOOLEAN => 'boolean',
+            self::LIST => 'array',
+        };
+    }
 }
