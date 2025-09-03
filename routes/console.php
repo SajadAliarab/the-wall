ba89bcb197f87f2320b954ab88916a7633bfa243
Sajad Aliarab <sajad.aliarab@gmail.com>
2025-09-03 09:16:07 +0330

    feat(post): daily force delete command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Console\Commands\DeletePosts;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
+Schedule::command(DeletePosts::class)
+    ->daily();
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
