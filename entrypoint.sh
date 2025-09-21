2557afa4ca7371e8dbb21f187e5a277013482ec8
Sajad Aliarab <sajad.aliarab@gmail.com>
2025-09-21 19:31:15 +0100

    fix(docker): dockerize the project database name

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,18 @@
 set -e
 
 
-if [ ! -f /var/www/thewall.sqlite ]; then
-    touch /var/www/thewall.sqlite
-    chown www-data:www-data /var/www/thewall.sqlite
-    echo "SQLite database created: /var/www/thewall.sqlite"
+if [ ! -f /var/www/thewall ]; then
+    touch /var/www/thewall
+    chown www-data:www-data /var/www/thewall
+    echo "SQLite database created: /var/www/thewall"
 fi
 
 if [ ! -d /var/www/vendor ]; then
     composer install --no-interaction --prefer-dist --optimize-autoloader
 fi
 
-
 php /var/www/artisan migrate --force || echo "Migration failed (maybe already migrated)"
 php /var/www/artisan db:seed --class=DevSeeder --force || echo "Seeding failed (maybe already seeded)"
-
-
 php /var/www/artisan storage:link || true
 php /var/www/artisan config:clear || true
 php /var/www/artisan cache:clear || true
